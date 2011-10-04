71050a6265047c40abeae8883fb87cb18b56bb32
Eloy Espinaco <eloyesp@gmail.com>
2011-10-04 17:01:45 -0300

    ADD: Unit

--- app/models/item.rb
+++ app/models/item.rb
@@ -1,2 +1,5 @@
 class Item < ActiveRecord::Base
+  belongs_to :product
+  belongs_to :compra
+  belongs_to :unit
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20111004194429) do
+ActiveRecord::Schema.define(:version => 20111004195912) do
 
   create_table "active_admin_comments", :force => true do |t|
     t.integer  "resource_id",   :null => false
@@ -84,6 +84,12 @@ ActiveRecord::Schema.define(:version => 20111004194429) do
   end
 
   create_table "products", :force => true do |t|
+    t.string   "name"
+    t.datetime "created_at"
+    t.datetime "updated_at"
+  end
+
+  create_table "units", :force => true do |t|
     t.string   "name"
     t.datetime "created_at"
     t.datetime "updated_at"
