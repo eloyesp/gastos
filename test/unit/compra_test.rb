768749665937f4d3bff96ebf4f2dd0c9d7e80361
Eloy Espinaco <eloyesp@gmail.com>
2011-10-04 17:25:03 -0300

    TEST: Compra (asociaciones)

--- test/unit/compra_test.rb
+++ test/unit/compra_test.rb
@@ -1,7 +1,11 @@
 require 'test_helper'
 
 class CompraTest < ActiveSupport::TestCase
-  # test "the truth" do
-  #   assert true
-  # end
+  test "asociaciones" do
+    @compra = compras(:one)
+    assert_kind_of Place, @compra.place
+    @compra.items.each do |item|
+      assert_kind_of Item, item
+    end
+  end
 end
