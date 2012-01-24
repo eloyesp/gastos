41c81499ae32c6f1d8b9473066d03aa424f12db4
Eloy Espinaco <eloyesp@gmail.com>
2012-01-24 12:53:55 -0300

    Initial Commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gastos::Application.config.secret_token = '15ba44c8cccd70a34d095a12ddb9c587d7c81bdbf691b4c500c68303b52c33b09c7ce82d301b6cf5f882fb60372ca2bad967449f0d6bed2254a7bc85f31b791f'
